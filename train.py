aa176d1e18f2da905c1884078c8c6cbb949aa4d9
er-ssmohanty <pie2001@protonmail.com>
2022-11-09 23:14:30 +0530

    ..zombie del

--- train.py
+++
@@ -1,6 +0,0 @@
-model.fit_generator(
-    train_generator,
-    steps_per_epoch = train_generator.samples // batch_size,
-    validation_data = validation_generator, 
-    validation_steps = validation_generator.samples // batch_size,
-    epochs = nb_epochs)
